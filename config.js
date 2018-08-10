71e33fc4c96b9d83187140b33a13538a00cb5262
goga-m <goga.mamoulasvili@gmail.com>
2018-08-10 18:52:38 +0300

    New commands:
    * Issue, monitor & transfer custom eos tokens
    * Deploy token contract code (setcode)
    
    * Buy Ram
    * Get currency stats
    * Get contract code (getcode)
    * Delegate bandwidth (stake)
    * Get table rows
    * Account actions (history)

--- config.js
+++ config.js
@@ -1,22 +1,27 @@
+const ololog = require('ololog')
+
 module.exports = {
   // Eosjs configuration options
   eosConfig: {
     chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca', // 32 byte (64 char) hex string
-    keyProvider: '5JbXQFidmVzvjwScLnhjHwPmgrsRggutHMdZ6HQ2dw2xz264Sot',
-    httpEndpoint: 'http://193.93.219.219:8888',
+    httpEndpoint: 'https://jungle.eosio.cr',
     expireInSeconds: 60,
-    verbose: false,
+    verbose: true,
     broadcast: true,
-    sign: true
+    sign: true,
+    logger: { // Default logging functions
+      log: null,
+      error: ololog.red.error.noLocate
+    }
     // mockTransactions: 'pass'
   },
   accounts: [{
-    name: 'eosjsclitest',
-    pubkey: 'EOS6s3Dctsscgsky8AwwPKir3CyG2w4vwZMPnSNsGactKcR53nr9S',
-    privateKey: '5JbXQFidmVzvjwScLnhjHwPmgrsRggutHMdZ6HQ2dw2xz264Sot'
-  }, {
-    name: 'dkubpsnzaewh',
-    pubkey: 'EOS6Z1TVUcfuLF6VRHTqvUnpsfg9gdGXXepWUX5w264pmut5KMEzd',
-    privateKey: '5HrkduVcADgJPsx16R5s8YEDjH3ZazR8FxPece3dn1grGn4kvaR'
+    name: 'axuemtbilnbm',
+    pubkey: 'EOS5yHaXWDVR87ZMVdLP3QRNvuH7SMycbKKwJVyHeR9qYh3SnVjwS',
+    privateKey: '5KinmBLDSV2VWisU9xDtsS2VNtDKFtW5CFazXsAnGpjiTXHie3D'
+  }],
+  tokens: [{
+    symbol: 'DEFADCA',
+    code: 'axuemtbilnbm'
   }]
 }
