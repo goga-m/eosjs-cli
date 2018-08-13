e733f57283736afb12b3fcc5ca5035a72a46f400
goga-m <goga.mamoulasvili@gmail.com>
2018-08-13 11:06:28 +0300

    Ui changes
    * Format main menu
    * Add new loggers for promises (spinners)

--- config.js
+++ config.js
@@ -1,27 +1,24 @@
-const ololog = require('ololog')
+// const ololog = require('ololog')
 
 module.exports = {
   // Eosjs configuration options
   eosConfig: {
     chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca', // 32 byte (64 char) hex string
-    httpEndpoint: 'https://jungle.eosio.cr',
+    httpEndpoint: 'http://jungle.cryptolions.io:18888',
     expireInSeconds: 60,
     verbose: true,
     broadcast: true,
     sign: true,
     logger: { // Default logging functions
       log: null,
-      error: ololog.red.error.noLocate
+      error: null
     }
-    // mockTransactions: 'pass'
+    // mocktransactions: 'pass'
   },
   accounts: [{
-    name: 'axuemtbilnbm',
-    pubkey: 'EOS5yHaXWDVR87ZMVdLP3QRNvuH7SMycbKKwJVyHeR9qYh3SnVjwS',
-    privateKey: '5KinmBLDSV2VWisU9xDtsS2VNtDKFtW5CFazXsAnGpjiTXHie3D'
+    name: 'dwtkwxmqsmrj',
+    privateKey: '5KhpkdHYiEr14MGD5UujQ1tY4iH2b7CFJvWzHayXYFMemJxofp8',
+    pubkey: 'EOS8TyZnNhBu2w5KGE839wV3GNxtZ4Q86oBK2qddznfL2SzV11Eoa'
   }],
-  tokens: [{
-    symbol: 'DEFADCA',
-    code: 'axuemtbilnbm'
-  }]
+  tokens: []
 }
